cf73a266b276c75009171b80b36e3d9d73f03b58
Andrey Garnoshevich <garnoshevich@alkor.co.ru>
2026-01-06 16:41:19 +0300

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Andrei Harnashevich on 6.01.26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
